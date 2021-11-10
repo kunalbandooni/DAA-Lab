25ad4d2abfb87f2271b9ed557fab0f247226836a
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-11-10 20:48:34 +0530

    Added a lot of files :)

---
+++ Week 9/w9q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int w[n],v[n];
+    int max_val;
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>w[i];
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    cin>>max_val;
+    return 0;
+}
