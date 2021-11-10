49538109758ad73bf229a38f7d88c05ad4ee7cdf
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-11-10 21:20:29 +0530

    Uploaded a few more codes

--- Week 9/w9q2.cpp
+++ Week 9/w9q2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int w[n],v[n];
+    int w[n], v[n];
     int max_val;
     cin>>n;
     for(int i=0;i<n;i++)
@@ -11,5 +11,6 @@ int main(){
     for(int i=0;i<n;i++)
         cin>>v[i];
     cin>>max_val;
+    
     return 0;
 }
