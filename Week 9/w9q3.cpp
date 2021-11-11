1958a25fa02b5abff027983f09a3615b0abadd6e
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-11-11 15:19:38 +0530

    Added Week 9 Q3

--- Week 9/w9q3.cpp
+++ Week 9/w9q3.cpp
@@ -11,9 +11,16 @@ int main(){
         a.push_back(t);
     }
     sort(a.begin(),a.end());
-    int sum=0;
-    for(int i=0;i<n;i++)
-        sum+=a[i];
+    int sum=a[0]+a[1];
+    vector<int> s;
+    s.push_back(sum);
+    for(int i=2;i<n;i++){
+        sum=sum+a[i];
+        s.push_back(sum);
+    }
+    sum=0;
+    for(int i=0;i<s.size();i++)
+        sum+=s[i];
     cout<<"OUTPUT:"<<endl<<sum;
     return 0;
 }
